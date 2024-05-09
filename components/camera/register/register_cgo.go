2d9f5895e42d65f42e5c7919c80e73ecda97b582
jeremyrhyde <jeremy@viam.com>
2024-05-09 15:01:00 -0400

    videosoruce removal

--- components/camera/register/register_cgo.go
+++ components/camera/register/register_cgo.go
@@ -10,5 +10,4 @@ import (
 	_ "go.viam.com/rdk/components/camera/replaypcd"
 	_ "go.viam.com/rdk/components/camera/ultrasonic"
 	_ "go.viam.com/rdk/components/camera/velodyne"
-	_ "go.viam.com/rdk/components/camera/videosource"
 )
